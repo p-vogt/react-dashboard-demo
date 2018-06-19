3ad2aae8ffbd59298912e56fbecf35f43d013dca
Patrick <patrick.vogt@fh-bielefeld.de>
2018-06-19 20:32:50 +0200

    add Paper with styling

--- iot-dashboard-demo/src/components/TemperatureLineChart/TemperatureLineChart.js
+++ iot-dashboard-demo/src/components/TemperatureLineChart/TemperatureLineChart.js
@@ -1,81 +1,88 @@
 import React, { Component } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import openSocket from 'socket.io-client';
+import Paper from '@material-ui/core/Paper';
 
 class TemperatureLineChart extends Component {
 
     constructor(props) {
         super(props);
-            this.addData = this.addData.bind(this)
-            const dataService = openSocket('http://localhost:5000/user');
-        
-            dataService.on('new-data', (val) => this.addData(val));
-        
-            dataService.on('error', (error) => {
-              console.log(error);
-            });
+        this.addData = this.addData.bind(this)
+        const dataService = openSocket('http://localhost:5000/user');
+
+        dataService.on('new-data', (val) => this.addData(val));
+
+        dataService.on('error', (error) => {
+            console.log(error);
+        });
 
         let data = [
-            { name: 'Page A', temp1: 30, temp2: 25 },
-            { name: 'Page A', temp1: 31, temp2: 25 },
-            { name: 'Page A', temp1: 32, temp2: 25 },
-            { name: 'Page A', temp1: 33, temp2: 25 },
-            { name: 'Page A', temp1: 29, temp2: 25 },
-            { name: 'Page A', temp1: 28, temp2: 25 },
-            { name: 'Page A', temp1: 31, temp2: 25 },
-            { name: 'Page A', temp1: 33, temp2: 25 },
-            { name: 'Page A', temp1: 30, temp2: 25 },
-            { name: 'Page A', temp1: 35, temp2: 25 },
-            { name: 'Page A', temp1: 30, temp2: 25 },
-            { name: 'Page A', temp1: 34, temp2: 25 },
-            { name: 'Page A', temp1: 35, temp2: 25 },
-            { name: 'Page A', temp1: 32, temp2: 25 },
-            { name: 'Page A', temp1: 28, temp2: 25 },
-            { name: 'Page A', temp1: 27, temp2: 25 },
-            { name: 'Page A', temp1: 26, temp2: 25 },
-            { name: 'Page A', temp1: 25, temp2: 25 },
-            { name: 'Page A', temp1: 29, temp2: 25 },
-            { name: 'Page A', temp1: 30, temp2: 25 },
-            { name: 'Page A', temp1: 29, temp2: 25 },
-            { name: 'Page A', temp1: 35, temp2: 25 },
-            { name: 'Page A', temp1: 34, temp2: 25 },
-            { name: 'Page A', temp1: 31, temp2: 25 },
-            { name: 'Page A', temp1: 32, temp2: 25 },
-            { name: 'Page A', temp1: 33, temp2: 25 },
-            { name: 'Page A', temp1: 30, temp2: 25 },
-            { name: 'Page A', temp1: 28, temp2: 25 },
+            { name: 'Page A', Room1: 30, Room2: 25 },
+            { name: 'Page A', Room1: 31, Room2: 25 },
+            { name: 'Page A', Room1: 32, Room2: 25 },
+            { name: 'Page A', Room1: 33, Room2: 25 },
+            { name: 'Page A', Room1: 29, Room2: 25 },
+            { name: 'Page A', Room1: 28, Room2: 25 },
+            { name: 'Page A', Room1: 31, Room2: 25 },
+            { name: 'Page A', Room1: 33, Room2: 25 },
+            { name: 'Page A', Room1: 30, Room2: 25 },
+            { name: 'Page A', Room1: 35, Room2: 25 },
+            { name: 'Page A', Room1: 30, Room2: 25 },
+            { name: 'Page A', Room1: 34, Room2: 25 },
+            { name: 'Page A', Room1: 35, Room2: 25 },
+            { name: 'Page A', Room1: 32, Room2: 25 },
+            { name: 'Page A', Room1: 28, Room2: 25 },
+            { name: 'Page A', Room1: 27, Room2: 25 },
+            { name: 'Page A', Room1: 26, Room2: 25 },
+            { name: 'Page A', Room1: 25, Room2: 25 },
+            { name: 'Page A', Room1: 29, Room2: 25 },
+            { name: 'Page A', Room1: 30, Room2: 25 },
+            { name: 'Page A', Room1: 29, Room2: 25 },
+            { name: 'Page A', Room1: 35, Room2: 25 },
+            { name: 'Page A', Room1: 34, Room2: 25 },
+            { name: 'Page A', Room1: 31, Room2: 25 },
+            { name: 'Page A', Room1: 32, Room2: 25 },
+            { name: 'Page A', Room1: 33, Room2: 25 },
+            { name: 'Page A', Room1: 30, Room2: 25 },
+            { name: 'Page A', Room1: 28, Room2: 25 },
         ];
         this.state = {
             data
         }
-}
-
-addData(val) {
-    let data = this.state.data.slice(0);
-    if (data.length >= 30) {
-        data = data.slice(1, 30);
     }
-    data.push({
-        name: new Date().toISOString().substring(11, 20),
-        temp1: data[data.length - 1].temp1 + val,
-    });
-    this.setState({ data });
-};
 
-render() {
-    return (
-        <LineChart width={600} height={300} data={this.state.data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" animationDuration={0} dataKey="temp1" stroke="#8884d8" activeDot={{ r: 8 }} />
-            <Line type="monotone" animationDuration={0} dataKey="temp2" stroke="#0084d8" activeDot={{ r: 8 }} />
-        </LineChart>
-    );
-}
+    addData(val) {
+        let data = this.state.data.slice(0);
+        if (data.length >= 30) {
+            data = data.slice(1, 30);
+        }
+        data.push({
+            name: new Date().toISOString().substring(11, 20),
+            Room1: data[data.length - 1].Room1 + val,
+        });
+        this.setState({ data });
+    };
+
+    render() {
+        return (
+            <Paper style={{ width: '600px', margin: '10px 10px 10px 20px' }}>
+                <h1>
+                    Temperature
+                </h1>
+                <LineChart width={600} height={300} data={this.state.data}
+                    style={{ marginLeft: '-30px', marginTop: '-20px' }}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" animationDuration={0} dataKey="Room1" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" animationDuration={0} dataKey="Room2" stroke="#0084d8" activeDot={{ r: 8 }} />
+                </LineChart>
+            </Paper>
+        );
+    }
 }
 
 export default TemperatureLineChart;
